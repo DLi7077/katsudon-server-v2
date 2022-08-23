068035e490235b15b57c2d69a39747b77e95cb5d
butterriolu <derpvin12@gmail.com>
2022-08-23 14:00:34 -0400

    added indexes

--- src/database/models/Solution.ts
+++ src/database/models/Solution.ts
@@ -50,9 +50,9 @@ export const SolutionSchema: Schema<SolutionAttributes> = new Schema({
   }
 });
 
-SolutionSchema.index({
-  solution_language: 1
-});
+SolutionSchema.index({ user_id: 1 });
+SolutionSchema.index({ created_at: 1 });
+SolutionSchema.index({ solution_language: 1 });
 
 const SolutionModel: Model<SolutionAttributes> = mongoose.model(
   'Solution',
